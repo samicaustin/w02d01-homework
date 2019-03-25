6ecbaa2a7a06ec5d63a19f6731be1b006a9bf1fe
Sami Austin <samicaustin@gmail.com>
2019-03-25 15:28:21 -0600

    Commit 1 - Data types completed

---
+++ js/app.js
@@ -0,0 +1,39 @@
+// console.log("Homework");
+
+// // 1. DATA TYPES
+// // Light switch: boolean
+// const lightsOn = true;
+// // Email: string
+// const email = "ilovetheinternet@email.com";
+// // Spaceship = array
+// const spaceship = ["hull", "laser blasters", "tractor beam", "warp drive"];
+// // Student names: array
+// const students = ["Sami", "Jake", "Calvin", "Veda"];
+// // Student names & locations: array of objects
+// const studentNamesLocations = [
+//     {
+//         name: "Sami",
+//         location: "Denver"
+//     },
+//     {
+//         name: "Jake",
+//         location: "Denver"
+//     },
+//     {
+//         name: "Calvin",
+//         location: "Denver"
+//     },
+//     {
+//         name: "Veda",
+//         location: "Denver"
+//     }
+// ];
+// // student names, locations, and favorite tv shows
+// const studentNamesLocationsShows = [
+//     {
+//         name: "Sami",
+//         location: "Denver",
+//         show: "Adventure Time"
+//     },
+//     etc.
+// ]
