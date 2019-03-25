941dd836ab54c3fabc55e6e8d82471ab465c1e76
Sami Austin <samicaustin@gmail.com>
2019-03-25 15:44:44 -0600

    Commit 3 - crazyObject completed

--- js/app.js
+++ js/app.js
@@ -48,3 +48,42 @@
 //     favoriteDataType: "object"
 // };
 // console.log(sami.hobby);
+
+// // 3. CRAZY OBJECT
+// const crazyObject = {
+//     taco: [
+//       {
+//         meat: 'steak',
+//         cheese: ['panela', 'queso', 'chihuahua']
+//       },
+//       {
+//         meat: 'chicken',
+//         salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]
+//       },
+//     ],
+//     larry: {
+//       nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+//       quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+//       characters: [
+//         {
+//           name: "Jeff",
+//           occupation: "manager"
+//         },
+//         {
+//           name: "funkhauser",
+//           occupation: "tv dude"
+//         },
+//         {
+//           name: "susie",
+//           occupation: "jeffs wife",
+//           favourtieHobby: "Swearing at Larry and Jeff"
+//         },
+//       ]
+//     }
+//   };
+// const taco = crazyObject.taco;
+// console.log(taco[1].salsa[5]);
+// const larry = crazyObject.larry;
+// console.log(larry.quotes[0]);
+// console.log(larry.characters[2].favourtieHobby);
+// console.log(larry.characters[1]);
